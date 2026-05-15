20c92bf3b925e56694d06d71fc97f7c961aace17
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-05-15 23:29:00 +0530

    Basic Server Setup

---
+++ Backend/Server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  return res.send("Server running successfully");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on http://localhost:3000");
+});
